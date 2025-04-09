a2380723fe2e77a8a295bfbc0f4e64c84438bd53
Harilanto <raharisonhfanambinantsoa@gmail.com>
2025-04-09 13:21:42 +0300

    choose-players modals

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,33 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  inject,
+  signal,
+  ViewChild,
+} from '@angular/core';
 import { AssetsUrls } from '../../constants/AssetsUrl';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { ChoosePlayersModalComponent } from '../../components/choose-players-modal/choose-players-modal.component';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, MatIconModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    ChoosePlayersModalComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -21,11 +42,27 @@ export class HomeComponent {
   currentText = '';
   counter = 0;
   private intervalId: any;
+  readonly dialog = inject(MatDialog);
+  readonly players = signal('');
 
   ngOnInit() {
     setTimeout(() => {
       this.animateText();
     }, 1000);
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(ChoosePlayersModalComponent, {
+      data: { players: this.players },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+      if (result !== undefined) {
+        this.players.set(result);
+        console.log('mihidy', this.players);
+      }
+    });
   }
 
   animateText() {
