4d1ad753f1bac99eb5f77b66db0714d0fbe67212
Harilanto <raharisonhfanambinantsoa@gmail.com>
2026-01-31 23:53:53 +0300

    feat - goback home and setStarCase functions

--- src/app/interfaces/IPawn.ts
+++ src/app/interfaces/IPawn.ts
@@ -2,13 +2,15 @@ import { PLAYER_COLOR } from '../enums/PlayerColor.enum';
 import type { ICase } from './ICase';
 
 export interface IPawn {
-  id?: string;
+  id: string;
   idPlayer?: number;
-  color?: PLAYER_COLOR;
+  color: PLAYER_COLOR;
   previewsCase?: ICase;
   currentCase?: ICase;
   isSafe?: boolean;
   hasArrived?: boolean;
   isMoving?: boolean;
   isMoveable?: boolean;
+  startCase: ICase;
+  entryCase?: ICase;
 }
--- src/app/pages/game-board/game-board.component.ts
+++ src/app/pages/game-board/game-board.component.ts
@@ -25,6 +25,8 @@ import { PLAYER_COLOR } from '../../enums/PlayerColor.enum';
 import { DiceComponent } from '../../components/dice-component/dice.component';
 import { PawnComponent } from '../../components/pawn-component/pawn.component';
 import { IPawn } from '../../interfaces/IPawn';
+import { CASE_TYPE } from '../../enums/CaseType.enum';
+import { CdkObserveContent } from '@angular/cdk/observers';
 
 @Component({
   selector: 'app-game-board',
@@ -35,6 +37,7 @@ import { IPawn } from '../../interfaces/IPawn';
     PlayerZoneComponent,
     DiceComponent,
     PawnComponent,
+    CdkObserveContent,
   ],
   templateUrl: './game-board.component.html',
   styleUrl: './game-board.component.scss',
@@ -46,15 +49,22 @@ export class GameBoardComponent {
   diceValue = 1;
   turn: number = 0;
 
-  pawn: IPawn = {};
+  pawn: IPawn = {
+    id: '',
+    color: PLAYER_COLOR.BLUE,
+    startCase: { type: CASE_TYPE.COMMON, position: 27 },
+  };
 
   ngOnInit() {
     this.pawn = {
-      id: 'test',
+      id: 'BLUEpiece1',
       idPlayer: 0,
-      color: PLAYER_COLOR.GREEN,
+      color: PLAYER_COLOR.BLUE,
       isMoveable: false,
       isMoving: false,
+      currentCase: { type: CASE_TYPE.COMMON, position: 45 },
+      startCase: { type: CASE_TYPE.COMMON, position: 27 },
+      entryCase: { type: CASE_TYPE.COMMON, position: 25 },
     };
   }
 
@@ -96,5 +106,70 @@ export class GameBoardComponent {
 
   onPawnClick(pawn: IPawn) {
     console.log('test', pawn);
+  }
+
+  async onGoBackHome(pawn: IPawn) {
+    await this.placePawn(
+      pawn.id,
+      pawn.color,
+      pawn.currentCase?.type,
+      pawn.currentCase?.position,
+    );
+    await new Promise((resolve) => setTimeout(resolve, 50));
+    let current = pawn.currentCase?.position;
+    let startCase = pawn.startCase?.position;
+    if (current && current > 0 && startCase > 0) {
+      while (current != startCase) {
+        if (current == 1) {
+          current = 53;
+        }
+        await this.placePawn(
+          pawn.id,
+          pawn.color,
+          CASE_TYPE.COMMON,
+          current - 1,
+        );
+        await new Promise((resolve) => setTimeout(resolve, 50));
+        current--;
+      }
+      await this.placePawn(pawn.id, pawn.color, CASE_TYPE.COMMON, -1);
+    }
+  }
+
+  async placePawn(
+    pawnId: string,
+    pawnColor: PLAYER_COLOR,
+    caseType: CASE_TYPE | undefined,
+    position: number | undefined,
+  ) {
+    console.log('ato', pawnId, pawnColor, caseType, position);
+
+    let htmlPawnId = pawnId;
+    let place = document.getElementById(pawnColor + 'home' + pawnId);
+    if (position && position > 0) {
+      place =
+        caseType == CASE_TYPE.COMMON
+          ? document.getElementById('C' + position)
+          : document.getElementById(pawnColor + position);
+    } else {
+      place = document.getElementById(pawnColor + 'home' + pawnId.slice(-1));
+    }
+    const pawn = document.getElementById(htmlPawnId);
+    if (pawn && place) {
+      const rectPawn = pawn.getBoundingClientRect();
+      const rectCase = place.getBoundingClientRect();
+      pawn.style.top = rectCase.top - rectPawn.height / 2 + 'px';
+      pawn.style.left = rectCase.left + 'px';
+    }
+  }
+
+  async setStartCase(pawn: IPawn) {
+    await this.placePawn(
+      pawn.id,
+      pawn.color,
+      pawn.startCase?.type,
+      pawn.startCase?.position,
+    );
+    pawn.currentCase = pawn.startCase;
   }
 }
